e3f818c100c8ec5fcbed1dcd3c0e351319b0fc47
eiu165 <eiu165@gmail.com>
2012-02-24 10:50:12 -0600

    first checkin

---
+++ src/Website/Models/RestHttpVerbFilter.cs
@@ -0,0 +1,14 @@
+﻿    using System.Web.Mvc;
+
+    namespace Website.Models
+    {
+        public class RestHttpVerbFilter : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                var httpMethod = filterContext.HttpContext.Request.HttpMethod;
+                filterContext.ActionParameters["httpVerb"] = httpMethod;
+                base.OnActionExecuting(filterContext);
+            }
+        }
+    }
